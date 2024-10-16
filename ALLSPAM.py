447d65211c5a8d836a50ec83510cdbfd3c858d3e
mymultiplatform <95411163+mymultiplatform@users.noreply.github.com>
2024-10-16 16:23:18 -0700

    Create ALLSPAM.py

---
+++ ALLSPAM.py
@@ -0,0 +1,55 @@
+myma all media 
+
+Python
+
+ALL SPAM 
+USA-MXN
+
+
+revenue farm 
+incom farm 
+
+all media puechase 
+
+all media currency 
+
+media e.g youtube. instagrm. discord. x. tiktok. etsy. 
+
+mega loop:
+
+webscraping 
+image generator ai
+video generator ai 
+mp4 generator 
+mp3 generator 
+text to speech file 
+
+canvas 
+web media frames all.
+
+ALL types of frame stardards. Frame std: Rectagle. UI text 
+Dynamic Text. Dynamic Effects. 
+
+All Spam Types: Tiktok video uploader. Instagram commentor. Instagram Liker. Instagram Publisher. Tiktok comentor. Tiktok liker. Instagram fallow 
+X comentor. X liker. X Follower. Youtube commentor. Youtube liker. Youtube Uploader. Etsy Publisher manager. Paypal products manager. Facebook comment. Facebook Publisher. Facebook Liker.
+
+COMPLEX Spam Types: Discord course. Discord renvenue manager. Discord ad multiplatform media. Web Page UNIQUE SPAM. Ultra Webpage SPAM. REAL PROJECT SPAM. Architecture SPAM. THE GAME SPAM. Mymultiplatform.com spam. 
+
+All spam types & COMPLEX Spam Types. Especial function. CROSS PLATFORM SPAM. AUTO CLUSTER SPAM. 3 group media. 2 group media 4 group media. Check Compatibility.
+
+Spanish & English. Based on Location. Select Location. Both Locations Same Type. Experimental Location. USD. MXN.
+
+ALL IN ONE PYTHON UI. ONE WINDOW. Simple Window. 
+SPAM POST SUGGESTIONS.
+
+ALL SELECTIONS PANELS. All selections array. 
+all generated. Ready Load. Display. Select. 
+
+UI. Approve.
+Delete on Dissaprove 
+
+
+MASTER EXECUTOR SENDER. SEND APPROVED ARRAY.
+
+PUBLISH SEND SUCCESSFULL. 
+
